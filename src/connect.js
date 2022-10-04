88f655c3e9a425ce17c50d689bd038973f0d53ae
Unfamous Dyson <dougdyson@gmail.com>
2022-10-04 15:57:52 -0700

    remove unused files

--- src/connect.js
+++
@@ -1,9 +0,0 @@
-async function connect () {
-  console.log('Connecting to MetaMask...');
-  await ethereum.request({
-    method: 'wallet_enable',
-    params: [{
-      wallet_snap: { [snapId]: {} },
-    }]
-  })
-}
