74d652fbd3b8462daabfeb12b799087e0216e99d
Unfamous Dyson <dougdyson@gmail.com>
2022-10-19 13:30:32 -0700

    form setup to signing step

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
-app.get('/', express.static('public'));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
