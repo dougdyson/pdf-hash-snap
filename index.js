c9d6620639d0dd7bfe98042e30bd1212dec43c4a
Unfamous Dyson <dougdyson@gmail.com>
2022-10-19 23:42:52 -0700

    add fileUpload package + script assignments

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const express = require('express');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
