33bbee7bb25b0c02e5b39f097ed8ac72b0242cff
Unfamous Dyson <dougdyson@gmail.com>
2022-10-21 00:00:25 -0700

    scaffolding forr POST

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const path = require('path');
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const pdfParse = require('pdf-parse');
 
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
+
+app.post('/extract-text', (req, res) => {
+  console.log('pdfFile:', req.body.pdfFile);
+  res.send({ pdfFile: req.body.pdfFile });
+
+  // if (!req.files && !req.files.pdfFile) {
+  //   res.status(400);
+  //   res.end();
+  // }
+
+  // pdfParse(req.files.pdfFile).then((result) => {
+  //   res.send(result.text);
+  // });
+});
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
